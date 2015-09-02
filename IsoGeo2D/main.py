f0608e4cffa1867994f7b2b15cf6d977c3add330
ikoddn <ikonage@gmail.com>
2015-09-02 12:35:50 +0200

    Moved Newton testcode to main

--- IsoGeo2D/main.py
+++ IsoGeo2D/main.py
@@ -1,5 +1,7 @@
 import math
+import newton
 import numpy as np
+import pylab as plt
 from plotter import Plotter
 from splines import Spline2D
 
@@ -34,3 +36,52 @@ def test2():
     
     plotter = Plotter()
     plotter.plot(f, 10, 10)
+
+def f(u):
+    return np.array([u, u**3])
+
+def df(u):
+    return np.array([1, 3*u**2])
+
+def g(v):
+    return np.array([v, -50*v+100])
+
+def dg(v):
+    return np.array([1, -50])
+
+def h(u, v):
+    return f(u) - g(v)
+
+def hJacob(u, v):
+    dfu = df(u)
+    dgv = dg(v)
+    return np.matrix([[dfu[0], -dgv[0]], [dfu[1], -dgv[1]]])
+
+def testNewton():
+    u = np.linspace(-10, 10, 100)
+    v = np.linspace(-10, 10, 100)
+    
+    fxValues = []
+    fyValues = []
+    gxValues = []
+    gyValues = []
+    
+    for i in range(len(u)):
+        [fx,fy] = f(u[i])
+        [gx,gy] = g(v[i])
+        fxValues.append(fx)
+        fyValues.append(fy)
+        gxValues.append(gx)
+        gyValues.append(gy)
+        
+        
+    plt.plot(fxValues, fyValues)
+    plt.plot(gxValues, gyValues)
+    
+    uGuess = 4
+    vGuess = 4
+    uvIntersect = newton.newtonsMethod2D(h, hJacob, uGuess, vGuess)
+    [xIntersect, yIntersect] = f(uvIntersect[0])
+    plt.plot(xIntersect, yIntersect, marker='x')
+    
+    plt.show()
--- IsoGeo2D/newton.py
+++ IsoGeo2D/newton.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pylab as plt
 from scipy.linalg import solve
 
 def newtonsMethod1D(f, df, x, tolerance):
@@ -19,25 +17,6 @@ def newtonsMethod1D(f, df, x, tolerance):
 
 	return x
 
-def f(u):
-	return np.array([u, u**3])
-
-def df(u):
-	return np.array([1, 3*u**2])
-
-def g(v):
-	return np.array([v, -50*v+100])
-
-def dg(v):
-	return np.array([1, -50])
-
-def h(u, v):
-	return f(u) - g(v)
-
-def hJacob(u, v):
-	dfu = df(u)
-	dgv = dg(v)
-	return np.matrix([[dfu[0], -dgv[0]], [dfu[1], -dgv[1]]])
 
 def newtonsMethod2D(f, fJacob, u, v, tolerance=0.00001, maxAttempts=100):
 	attempt = 1
@@ -58,34 +37,3 @@ def newtonsMethod2D(f, fJacob, u, v, tolerance=0.00001, maxAttempts=100):
 		return None
 		
 	return [u, v]
-
-
-def testplot():
-	u = np.linspace(-10, 10, 100)
-	v = np.linspace(-10, 10, 100)
-	
-	fxValues = []
-	fyValues = []
-	gxValues = []
-	gyValues = []
-	
-	for i in range(len(u)):
-		[fx,fy] = f(u[i])
-		[gx,gy] = g(v[i])
-		fxValues.append(fx)
-		fyValues.append(fy)
-		gxValues.append(gx)
-		gyValues.append(gy)
-		
-		
-	plt.plot(fxValues, fyValues)
-	plt.plot(gxValues, gyValues)
-	
-	uGuess = 4
-	vGuess = 4
-	uvIntersect = newtonsMethod2D(h, hJacob, uGuess, vGuess)
-	[xIntersect, yIntersect] = f(uvIntersect[0])
-	plt.plot(xIntersect, yIntersect, marker='x')
-	
-	plt.show()
-	
