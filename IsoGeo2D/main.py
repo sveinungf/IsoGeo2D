7e1b7d09c640e55a2aef56fbe7aa613a2ba3b5cb
ikoddn <ikonage@gmail.com>
2015-09-02 12:02:36 +0200

    Newton's method in 2D

--- IsoGeo2D/main.py
+++ IsoGeo2D/main.py
@@ -3,17 +3,20 @@ import numpy as np
 from plotter import Plotter
 from splines import Spline2D
 
-def spline():
+def makeSpline():
     p = 2
-    uKnots = [0, 0, 0, 0.2, 0.7, 1.1, 1.1, 1.1]
-    vKnots = [0, 0, 0, 0.3, 0.6, 1.1, 1.1, 1.1]
+    uKnots = [0, 0, 0, 0.2, 0.7, 1.01, 1.01, 1.01]
+    vKnots = [0, 0, 0, 0.3, 0.6, 1.01, 1.01, 1.01]
     coeffs = np.array([[[0.0,0.0], [0.1,0.2], [0.1,0.5], [0,0.9], [0.0,1.0]],
-                       [[0.25,0.0], [0.1 ,0.2], [0.8,0.2], [0.5,0.9], [0.25,1.0]],
+                       [[0.25,0.0], [0.1 ,0.2], [0.4,0.2], [0.5,0.9], [0.25,1.0]],
                        [[0.5,0.0], [0.5,0.2], [0.5,0.5], [0.6,0.9], [0.5,1.0]],
                        [[0.75,0.0], [0.7,0.2], [0.8,0.5], [0.7,0.9], [0.75,1.0]],
                        [[1.0,0.0], [0.9,0.2], [0.8,0.5], [0.9,0.9], [1.0,1.0]]])
-
-    spline = Spline2D(p, uKnots, vKnots, coeffs)
+    
+    return Spline2D(p, uKnots, vKnots, coeffs)
+    
+def spline():
+    spline = makeSpline()
     plotter = Plotter()
     
     plotter.plot(spline.evaluate, 10, 10)
--- IsoGeo2D/newton.py
+++ IsoGeo2D/newton.py
@@ -1,4 +1,8 @@
-def newtonsMethod(f, df, x, tolerance):
+import numpy as np
+import pylab as plt
+from scipy.linalg import solve
+
+def newtonsMethod1D(f, df, x, tolerance):
 	'''
 	f is the function f(x) and df its derivative
 	x is the first guess
@@ -14,3 +18,74 @@ def newtonsMethod(f, df, x, tolerance):
 		x = x1
 
 	return x
+
+def f(u):
+	return np.array([u, u**3])
+
+def df(u):
+	return np.array([1, 3*u**2])
+
+def g(v):
+	return np.array([v, -50*v+100])
+
+def dg(v):
+	return np.array([1, -50])
+
+def h(u, v):
+	return f(u) - g(v)
+
+def hJacob(u, v):
+	dfu = df(u)
+	dgv = dg(v)
+	return np.matrix([[dfu[0], -dgv[0]], [dfu[1], -dgv[1]]])
+
+def newtonsMethod2D(f, fJacob, u, v, tolerance=0.00001, maxAttempts=100):
+	attempt = 1
+	
+	while attempt < maxAttempts:
+		[fx, fy] = f(u,v)
+		x = solve(fJacob(u,v), [-fx, -fy])
+		[u1, v1] = x + [u, v]
+		
+		if abs(u1 - u) < tolerance and abs(v1 - v) < tolerance:
+			break
+		
+		u = u1
+		v = v1
+		++attempt
+		
+	if attempt == maxAttempts:
+		return None
+		
+	return [u, v]
+
+
+def testplot():
+	u = np.linspace(-10, 10, 100)
+	v = np.linspace(-10, 10, 100)
+	
+	fxValues = []
+	fyValues = []
+	gxValues = []
+	gyValues = []
+	
+	for i in range(len(u)):
+		[fx,fy] = f(u[i])
+		[gx,gy] = g(v[i])
+		fxValues.append(fx)
+		fyValues.append(fy)
+		gxValues.append(gx)
+		gyValues.append(gy)
+		
+		
+	plt.plot(fxValues, fyValues)
+	plt.plot(gxValues, gyValues)
+	
+	uGuess = 4
+	vGuess = 4
+	uvIntersect = newtonsMethod2D(h, hJacob, uGuess, vGuess)
+	[xIntersect, yIntersect] = f(uvIntersect[0])
+	plt.plot(xIntersect, yIntersect, marker='x')
+	
+	plt.show()
+	
