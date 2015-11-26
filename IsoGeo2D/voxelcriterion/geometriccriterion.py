4db49d725ec2f07a204cf47fdd25a14a681c4219
ikoddn <ikonage@gmail.com>
2015-11-26 13:57:42 +0100

    Comparing the pixel frustum width against the diagonal of a voxel

--- IsoGeo2D/voxelcriterion/geometriccriterion.py
+++ IsoGeo2D/voxelcriterion/geometriccriterion.py
@@ -1,9 +1,11 @@
+import math
+
 from voxelcriterion import VoxelCriterion
 
 class GeometricCriterion(VoxelCriterion):
     def __init__(self, pixelWidth, voxelWidth):
         self.pixelWidth = pixelWidth
-        self.voxelWidth = voxelWidth
+        self.voxelDiagonal = math.sqrt(2) * voxelWidth 
     
     def lodLevel(self, viewRay, viewRayParam):
         samplePoint = viewRay.evalFromPixel(viewRayParam)
@@ -11,7 +13,7 @@ class GeometricCriterion(VoxelCriterion):
         
         pixelFrustumWidth = self.pixelWidth * z / viewRay.near
         
-        if pixelFrustumWidth >= (1.5 * self.voxelWidth):
+        if pixelFrustumWidth >= (1.5 * self.voxelDiagonal):
             return 0
         else:
             return -1
