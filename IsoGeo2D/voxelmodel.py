fe2364ec2e3d44029e290d85811eee7ff0f6c303
ikoddn <ikonage@gmail.com>
2015-12-11 12:17:38 +0100

    Using the scalars at the intersection points when raycasting the voxel model

--- IsoGeo2D/voxelmodel.py
+++ IsoGeo2D/voxelmodel.py
@@ -1,5 +1,7 @@
 import math
+import numpy as np
 
+import fileio.splinereader
 import compositing
 from samplingtype import SamplingType
 
@@ -12,6 +14,8 @@ class VoxelModel:
         self.plotter = plotter
         self.scalarTexture = scalarTexture
         self.transfer = transfer
+        
+        self.rho = fileio.splinereader.read('datasets/0/rho.json')
     
     def sample(self, samplePoint):
         bb = self.boundingBox
@@ -38,43 +42,54 @@ class VoxelModel:
         sampleDeltas = []
         samplePoints = []
         sampleTypes = []
-
-        viewRayParam = 0
         
+        inParamPoint = intersections[0].paramPoint
         inGeomPoint = intersections[0].geomPoint
+        outParamPoint = intersections[1].paramPoint
         outGeomPoint = intersections[1].geomPoint
         
-        prevGeomPoint = None
+        scalar = self.rho.evaluate(inParamPoint[0], inParamPoint[1])
+        color = self.transfer(scalar)
+        sampleColors.append(color)
         
-        while viewRay.inRange(viewRayParam):
-            samplePoint = viewRay.evalFromPixel(viewRayParam)
+        samplePoints.append(inGeomPoint)
+        sampleTypes.append(SamplingType.SPLINE_MODEL)
+        
+        prevGeomPoint = inGeomPoint
+        
+        viewDirDelta = viewRay.viewDir * delta
+        samplePoint = inGeomPoint + viewDirDelta
+        
+        while samplePoint[0] < outGeomPoint[0]:
+            sampleColor = self.sample(samplePoint)
             
-            if samplePoint[0] > outGeomPoint[0]:
-                break
-            
-            if inGeomPoint[0] <= samplePoint[0]:
-                sampleColor = self.sample(samplePoint)
-                    
-                if sampleColor == None:
-                    sampleType = SamplingType.OUTSIDE_OBJECT
-                else:
-                    sampleColors.append(sampleColor)
-                    
-                    if prevGeomPoint != None:
-                        dist = samplePoint - prevGeomPoint
-                        sampleDeltas.append(math.sqrt(dist[0]**2 + dist[1]**2))
-                    
-                    sampleType = SamplingType.VOXEL_MODEL
-                    
-                    prevGeomPoint = samplePoint
-            else:
+            if sampleColor == None:
                 sampleType = SamplingType.OUTSIDE_OBJECT
+            else:
+                sampleType = SamplingType.VOXEL_MODEL
+                sampleColors.append(sampleColor)
+                
+                if prevGeomPoint != None:
+                    dist = samplePoint - prevGeomPoint
+                    sampleDeltas.append(math.sqrt(dist[0]**2 + dist[1]**2))
+                    
+                prevGeomPoint = np.array(samplePoint)
                 
             samplePoints.append(samplePoint)
             sampleTypes.append(sampleType)
             
-            viewRayParam += delta
+            samplePoint = samplePoint + viewDirDelta
         
+        scalar = self.rho.evaluate(outParamPoint[0], outParamPoint[1])
+        color = self.transfer(scalar)
+        sampleColors.append(color)
+        
+        dist = outGeomPoint - prevGeomPoint
+        sampleDeltas.append(math.sqrt(dist[0]**2 + dist[1]**2))
+        
+        samplePoints.append(outGeomPoint)
+        sampleTypes.append(SamplingType.SPLINE_MODEL)
+
         if plotter != None and self.plotSamplePoints:
             plotter.plotSamplePoints(samplePoints, sampleTypes)
         
