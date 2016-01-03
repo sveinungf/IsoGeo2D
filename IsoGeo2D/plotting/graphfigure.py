5aa107eb80aa275fb6bde55c7212feaae7055a66
sveinungf <ikonage@gmail.com>
2016-01-03 16:31:52 +0100

    Plotting graph for max and var for the voxelized model

--- IsoGeo2D/plotting/graphfigure.py
+++ IsoGeo2D/plotting/graphfigure.py
@@ -7,20 +7,36 @@ class GraphFigure:
         fig = plt.figure()
         self.fig = fig
 
-        mainGrid = plt.GridSpec(1, 1)
+        mainGrid = plt.GridSpec(2, 2)
 
         ax = fig.add_subplot(mainGrid[0, 0])
+        ax.set_xlabel('Texture size (n x n)')
+        ax.set_ylabel('Max')
+        self.maxGraph = GraphPlotter(ax)
+
+        ax = fig.add_subplot(mainGrid[0, 1])
         ax.set_xlabel('Texture size (n x n)')
         ax.set_ylabel('Mean')
         self.meanGraph = GraphPlotter(ax)
 
+        ax = fig.add_subplot(mainGrid[1, 0])
+        ax.set_xlabel('Texture size (n x n)')
+        ax.set_ylabel('Var')
+        self.varGraph = GraphPlotter(ax)
+
     def graphVoxelSummaries(self, texDimSizes, summaries):
+        maxes = []
         means = []
+        vars = []
 
         for summary in summaries:
+            maxes.append(summary.max)
             means.append(summary.mean)
+            vars.append(summary.var)
 
+        self.maxGraph.plotGraph(texDimSizes, maxes)
         self.meanGraph.plotGraph(texDimSizes, means)
+        self.varGraph.plotGraph(texDimSizes, vars)
 
     def show(self):
         self.fig.show()
