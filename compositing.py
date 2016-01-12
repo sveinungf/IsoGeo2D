95123fe74f5270fe38f53178eab6389b44699509
ikoddn <ikonage@gmail.com>
2016-01-12 17:23:29 +0100

    Replaced the accumulate() function to prevent cases where the resulting color never got saturated (alpha super close but lower than 1.0)

--- compositing.py
+++ compositing.py
@@ -12,12 +12,27 @@ def frontToBack(rgbaValues, deltas):
         
     return dst
 
+def accumulateOld(dst, src, delta):
+    xi = 0.01
+
+    t = (1 - src[3])**(delta/xi)
+    factor = (1 - t)*(1 - dst[3])
+
+    dst[:3] += factor * src[:3]
+    dst[3] += factor
+
+    return dst
+
+
 def accumulate(dst, src, delta):
     xi = 0.01
     
-    t = (1 - src[3])**(delta/xi)
-    factor = (1 - t)*(1 - dst[3])
-    
+    t = (1.0 - src[3])**(delta/xi)
+    factor = (1.0 - t)*(1.0 - dst[3])
+
+    if (1.0 - dst[3]) < 1e-10:
+        factor = 1.0 - dst[3]
+
     dst[:3] += factor * src[:3]
     dst[3] += factor
     
