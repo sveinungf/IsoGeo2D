af215354f7d3513e2ebb839b66303e81ad07aae6
ikoddn <ikonage@gmail.com>
2016-01-14 16:17:32 +0100

    Script for plotting geometry as SVG

---
+++ svggeometry.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from plotting.splineplotter import SplinePlotter
+from dataset import Dataset
+
+
+plt.figure(figsize=[6,6])
+dataset = Dataset(1, 1)
+splineInterval = np.array([0.0, 1.0])
+s = SplinePlotter(plt, splineInterval)
+s.plotGrid(dataset.phi.evaluate, 10, 10)
+plt.savefig("output/svg/geometry.svg", format="svg")
