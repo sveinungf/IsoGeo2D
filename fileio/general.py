857a764933d54a5dde4c90febe8561cbce454401
sveinungf <ikonage@gmail.com>
2016-02-07 15:10:22 +0100

    Specifying data set when saving rendering results

--- fileio/general.py
+++ fileio/general.py
@@ -3,23 +3,33 @@ import os
 import pickle
 
 
-def save(object, filename):
-    with open('output/results/{}.pkl'.format(filename), 'wb') as output:
+def __filedir(dataset):
+    return 'output/results/{},{}'.format(dataset.rhoNumber, dataset.phiNumber)
+
+def save(dataset, object, filename):
+    directory = __filedir(dataset)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    filepath = '{}/{}.pkl'.format(directory, filename)
+
+    with open(filepath, 'wb') as output:
         pickle.dump(object, output, pickle.HIGHEST_PROTOCOL)
 
-def load(filename):
-    #with open('output/results/{}'.format(filename), 'rb') as input:
-    #    return pickle.load(input)
-    f = file('output/results/{}'.format(filename), 'rb')
+def load(dataset, filename):
+    filepath = '{}/{}'.format(__filedir(dataset), filename)
+
+    f = file(filepath, 'rb')
     s = pickle.load(f)
     f.close()
 
     return s
 
-def findAll():
+def findAll(dataset):
     files = []
 
-    for file in os.listdir('output/results'):
+    for file in os.listdir(__filedir(dataset)):
         if file.endswith('.pkl'):
             files.append(file)
 
